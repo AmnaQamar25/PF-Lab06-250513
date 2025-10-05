99b99ee5e9b8157b54e1a5d1f02eced3c1ae528a
Amna qamar <k250513@nu.edu.pk>
2025-10-05 06:03:49 -0700

    Update lab6task4.c

--- lab6task4.c
+++ lab6task4.c
@@ -5,14 +5,15 @@ int main(){
 	printf("Enter a number : ");
 	scanf("%d", &num);
 	
-	printf("0\n");
+	printf("0 ");
 	--num;
 	while(i<num){
 		finalans = ans1 + ans2;
-		printf("%d\n", finalans); 
+		printf("%d ", finalans); 
 		ans2=ans1;
 		ans1 = finalans;
 		i++;
 	}
 	return 0;
 }
+
